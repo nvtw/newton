23e4da54cea33b93f7ccd07410e863746e56a2cb
twidmer <twidmer@nvidia.com>
2025-07-17 14:32:11 +0200

    Add safeguard for picking to support renderer that cannot handle picking
    Signed-off-by: twidmer <twidmer@nvidia.com>

--- newton/examples/example_quadruped.py
+++ newton/examples/example_quadruped.py
@@ -104,7 +104,8 @@ class Example:
     def simulate(self):
         for _ in range(self.sim_substeps):
             self.state_0.clear_forces()
-            self.renderer.apply_picking_force(self.state_0)
+            if self.renderer and hasattr(self.renderer, "apply_picking_force"):
+                self.renderer.apply_picking_force(self.state_0)
             self.contacts = self.model.collide(self.state_0)
             self.solver.step(self.state_0, self.state_1, self.control, self.contacts, self.sim_dt)
             self.state_0, self.state_1 = self.state_1, self.state_0
