1ddb52ea6300a8e88ae92c17e085d06bdc825d97
twidmer <twidmer@nvidia.com>
2025-07-17 10:20:01 +0200

    Implement picking
    Signed-off-by: twidmer <twidmer@nvidia.com>

--- newton/examples/example_quadruped.py
+++ newton/examples/example_quadruped.py
@@ -104,6 +104,7 @@ class Example:
     def simulate(self):
         for _ in range(self.sim_substeps):
             self.state_0.clear_forces()
+            self.renderer.apply_picking_force(self.state_0)
             self.contacts = self.model.collide(self.state_0)
             self.solver.step(self.state_0, self.state_1, self.control, self.contacts, self.sim_dt)
             self.state_0, self.state_1 = self.state_1, self.state_0
@@ -123,7 +124,7 @@ class Example:
         with wp.ScopedTimer("render"):
             self.renderer.begin_frame(self.sim_time)
             self.renderer.render(self.state_0)
-            self.renderer.render_contacts(self.state_0, self.contacts, contact_point_radius=1e-2)
+            self.renderer.render_contacts(self.state_0.body_q, self.contacts, contact_point_radius=1e-2)
             self.renderer.end_frame()
 
 
