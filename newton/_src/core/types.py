ff3862ca7fbad8893eb9c980799be69234e46b34
twidmer <twidmer@nvidia.com>
2025-09-09 08:26:15 +0200

    Remove one more white space change

--- newton/_src/core/types.py
+++ newton/_src/core/types.py
@@ -57,7 +57,6 @@ Mat33 = list[float] | wp.mat33
 Transform = tuple[Vec3, Quat] | wp.transform
 """A 3D transformation represented as a tuple of 3D translation and rotation quaternion (in XYZW order)."""
 
-
 # type alias for numpy arrays
 nparray = np.ndarray[Any, np.dtype[Any]]
 
