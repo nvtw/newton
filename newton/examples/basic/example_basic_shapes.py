43d8dc999ff42ba07dc13c9b46fb25ca3de81115
twidmer <twidmer@nvidia.com>
2025-11-21 09:35:20 +0100

    Revert "Ran ruff"
    This reverts commit c70b3540675635ba1da5878533ebdf3640229846.

--- newton/examples/basic/example_basic_shapes.py
+++ newton/examples/basic/example_basic_shapes.py
@@ -101,8 +101,7 @@ class Example:
         self.collision_pipeline = newton.examples.create_collision_pipeline(
             self.model,
             args,
-            rigid_contact_max_per_pair=100,
-        )
+            rigid_contact_max_per_pair=100,        )
         self.contacts = self.model.collide(self.state_0, collision_pipeline=self.collision_pipeline)
 
         self.viewer.set_model(self.model)
